23feee8d82e8646acc50d141935fbe715b5b850d
Claudio Jolowicz <cj@dealloc.org>
2014-07-11 18:05:08 +0200

    Support tarballs compressed with bz2 or xz.

--- lib/source.sh
+++ lib/source.sh
@@ -1,9 +1,30 @@
+_extract_tar_gz() {
+    tar zxf "$1"
+}
+
+_extract_tar_bz2() {
+    tar jxf "$1"
+}
+
+_extract_tar_xz() {
+    tar Jxf "$1"
+}
+
+_extract() {
+    case $1 in
+        *.tgz)     _extract_tar_gz  "$1" ;;
+        *.tar.gz)  _extract_tar_gz  "$1" ;;
+        *.tar.bz2) _extract_tar_bz2 "$1" ;;
+        *.tar.xz)  _extract_tar_xz  "$1" ;;
+    esac
+}
+
 do_source() {
     cd $srcdir
 
     wget "${wget_opts[@]}" -O $file $url
 
-    tar zxf $file
+    _extract $file
 }
 
 commands+=(source)
